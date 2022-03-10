cc86faa8ee68284456d564c40f31830fb2efdd95
joshua530 <joshuaomari5@gmail.com>
2022-03-10 22:16:02 +0300

    Implement betty requirements

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints out a simple string
+ *
+ * it uses the puts function to print out the string
+ *
+ * Return: the exit status of the program
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
