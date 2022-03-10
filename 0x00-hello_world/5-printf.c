7368d9184eea9e254b86300bcb03fa5abac30452
joshua530 <joshuaomari5@gmail.com>
2022-03-10 22:27:09 +0300

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a string
+ *
+ * Description: uses printf to print out a simple string
+ *
+ * Return: the exit value of the program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
