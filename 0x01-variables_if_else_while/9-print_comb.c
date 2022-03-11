0d71383e48de6081c52b1099e0faf0c96e13a4e1
joshua530 <joshuaomari5@gmail.com>
2022-03-11 11:03:59 +0300

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints base 10 numbers
+ *
+ * Description: prints all single digit base 10 numbers without using
+ * any variable of type char
+ *
+ * Return: function's exit code
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (; i < 58; ++i)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(13);
+	return (0);
+}
