d24e68af7a51b979f6e70b82566e362fae17dc27
joshua530 <joshuaomari5@gmail.com>
2022-03-11 09:56:34 +0300

    Add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Description: prints out all lowercase letters followed by uppercase
+ * letters
+ *
+ * Return: exit code of the function
+ */
+int main(void)
+{
+	for (int i = 97; i < 123; ++i)
+		putchar(i);
+	for (int j = 65; j < 91; ++j)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
