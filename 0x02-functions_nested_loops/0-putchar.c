7f2688d1e4fe94bff51ac284a02654ff578312be
joshua530 <joshuaomari5@gmail.com>
2022-03-15 22:00:00 +0300

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "_putchar.c"
+
+/**
+ * main - prints out a string
+ *
+ * Description - uses _putchar imported function to print out a string
+ *
+ * Return: the exit code of the program
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void _putchar(const char *str)
+{
+  printf("%s\n", str);
+}
