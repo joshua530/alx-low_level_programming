dbb69bb770d95aeed87a4cb48730dca70fa5db38
joshua530 <joshuaomari5@gmail.com>
2022-03-16 00:43:23 +0300

    98 battery street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,6 +4,8 @@
 /**
  * print_to_98 - prints all natural numbers till 98
  *
+ * @a: the beginning point
+ *
  * Description: returns the result of adding two numbers
  *
  * Return: void
@@ -12,12 +14,25 @@ void print_to_98(int a)
 {
 	int i = a;
 
-	for (; i <= 98; ++i)
+	if (i <= 98)
 	{
-		if (i == a)
-			printf("%d", i);
-		else
-			printf(",%3d", i);
+		for (; i <= 98; ++i)
+		{
+			if (i == a)
+				printf("%d", i);
+			else
+				printf(",%3d", i);
+		}
+	}
+	else
+	{
+		for (; i >= 98; --i)
+		{
+			if (i == a)
+				printf("%d", i);
+			else
+				printf(",%3d", i);
+		}
 	}
 	printf("\n");
 }
