12e9d6e158b70103870d2f285e22a4e32e9b2122
joshua530 <joshuaomari5@gmail.com>
2022-03-16 00:31:16 +0300

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,9 +18,10 @@ void times_table(void)
 	{
 		for (; j < 10; ++j)
 		{
-			printf("%d", i * j);
-			if (j < 9)
-				printf(",  ");
+			if (j > 0)
+				printf(",%3d", i * j);
+			else
+				printf("%d", i * j);
 		}
 		printf("\n");
 		j = 0;
