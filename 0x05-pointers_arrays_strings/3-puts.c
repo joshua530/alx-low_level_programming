8e171192c7aa084517185c05b0fdc970899ac3db
joshua530 <joshuaomari5@gmail.com>
2022-03-21 20:40:36 +0300

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string to print out
+ *
+ * Description: prints a string to stdout
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		++str;
+	}
+}
