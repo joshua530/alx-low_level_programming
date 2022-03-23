23a43f2d5ed182f88e5e0421d799e425a48b8b64
joshua530 <joshuaomari5@gmail.com>
2022-03-23 20:15:01 +0300

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings, using at most n bytes
+ *
+ * @dest: the string that will be concatenated to
+ * @src: the string from which the bytes will be extracted
+ *
+ * Description: the source string does not need to have a null
+ * terminator if it exceeds n bytes
+ *
+ * Return: pointer to the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+  return (strncat(dest, src, n));
+}
