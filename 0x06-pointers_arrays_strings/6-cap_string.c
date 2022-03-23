81f962c808f5d41034a1d153a98e29a0d1a7c9d9
joshua530 <joshuaomari5@gmail.com>
2022-03-23 21:46:27 +0300

    bug fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
+
+bool exists(char *haystack, char needle);
 
 /**
  * cap_string - capitalizes words in string
@@ -15,13 +18,45 @@ char *cap_string(char *str)
 {
 	int len;
 	int i;
+	char separators[] = {
+			' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	len = strlen(str);
 	for (i = 0; i < len; ++i)
 	{
-		if (isalpha(str[i]))
+		/* we are at the beginning of a word */
+		if (isalpha(str[i]) &&
+				(i > 0) &&										 /* prevent segmentation error */
+				exists(separators, str[i - 1]) /* preceded by a punctuation character */
+		)
+		{
 			str[i] = toupper(str[i]);
+		}
 	}
 
 	return (str);
+}
+
+/**
+ * exists - checks whether a character exists in a string
+ *
+ * @haystack: the string to search
+ * @needle: the character to look for
+ *
+ * Description: looks for a character in a string
+ *
+ * Return: true if character exists, false if absent
+ */
+bool exists(char *haystack, char needle)
+{
+	int len;
+	int i;
+
+	len = strlen(haystack);
+	for (i = 0; i < len; ++i)
+	{
+		if (haystack[i] == needle)
+			return (true);
+	}
+	return (false);
 }
