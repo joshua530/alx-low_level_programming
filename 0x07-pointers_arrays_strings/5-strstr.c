11cdd26415f78b9f5edc26492d68ed4e644f9f6d
joshua530 <joshuaomari5@gmail.com>
2022-03-28 22:27:05 +0300

    add strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - search for occurence in string
+ *
+ * @haystack: string to be searched
+ * @needle: string to locate
+ *
+ * Description: search for occurence in string
+ *
+ * Return: pointer to the first occurrence or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    return (strstr(haystack, needle));
+}
