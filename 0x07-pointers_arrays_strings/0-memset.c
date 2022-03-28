0bada47d001bb25c2be3622ed18088b09b5f4f06
joshua530 <joshuaomari5@gmail.com>
2022-03-28 21:49:56 +0300

    make files betty compliant

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,18 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _memset - implementation of memset function
+ *
+ * @s: memory to fill
+ * @b: character to fill
+ * @n: number of bits to fill
+ *
+ * Description: fills memory address pointed to by s with b n times
+ *
+ * Return: pointer to memory address that has been filled
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-    return memset(s, b, n);
+	return (memset(s, b, n));
 }
