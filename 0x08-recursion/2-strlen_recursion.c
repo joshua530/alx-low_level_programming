bfe38f074369dbe8a8dfbbda90e27f40d95b374f
joshua530 <joshuaomari5@gmail.com>
2022-03-30 22:03:23 +0300

    add 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - gets string length
+ *
+ * @s: string to calculate tlength for
+ *
+ * Description: gets string length
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	return strlen(s);
+}
