5545146cdd8afe01aab92a99846b989c7404500c
joshua530 <joshuaomari5@gmail.com>
2022-03-30 22:25:59 +0300

    implement _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - finds the power of a number
+ *
+ * @x: the number to be raised to a given power
+ * @y: the power to raise the number to
+ *
+ * Description: finds the power of a number
+ * Return: power of the number
+ */
+int _pow_recursion(int x, int y)
+{
+	return (pow(x, y));
+}
