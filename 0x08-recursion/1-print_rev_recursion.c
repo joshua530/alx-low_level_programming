1f816201d1566df6f4b9e358f1e51fcfaae752f6
joshua530 <joshuaomari5@gmail.com>
2022-03-30 21:51:56 +0300

    add _print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_char - helper function for _print_rev_recursion
+ *
+ * @s: current character
+ * @current: current index
+ *
+ * Description: adds position tracking
+ * Return: void
+ */
+void print_char(char *s, int current)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	print_char(s, current + 1);
+	_putchar(*s);
+	if (current == 0)
+	{
+		_putchar('\n');
+	}
+}
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: the string to print
+ *
+ * Description: prints out a string in reverse
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	print_char(s, 0);
+}
