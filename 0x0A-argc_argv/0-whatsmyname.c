da63273323f92c09495fce941c275803b5e2a5e5
joshua530 <joshuaomari5@gmail.com>
2022-04-01 20:50:36 +0300

    add unused attribute annotation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  * Description: prints the name of the program
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(__attribute__((unused)) int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
