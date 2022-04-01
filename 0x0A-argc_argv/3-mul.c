bf7c07c5f0fa07c0b712389d124815f1814821e8
joshua530 <joshuaomari5@gmail.com>
2022-04-01 21:27:29 +0300

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program
+ *
+ * @argc: argument count
+ * @argv: array containing arguments passed to program
+ *
+ * Description: multiplies 2 numbers given as arguments
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	int first, last;
+	if (argv[1] && argv[2])
+	{
+		first = atoi(argv[1]);
+		last = atoi(argv[2]);
+		printf("%d-%d\n", first, last);
+		printf("%d\n", first * last);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
