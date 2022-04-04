061ef10c3e2548a21d647ecbeaa705f9d5286862
joshua530 <joshuaomari5@gmail.com>
2022-04-04 20:06:57 +0300

    fill array with given character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,13 +8,22 @@
  * @size: the number of characters to allocate memory for
  * @c: the characters that the array will be filled with
  *
- * Description: allocates a new memory block for characters
+ * Description: allocates a new memory block for characters and fills
+ * it with the given character
+ *
  * Return: pointer to the allocated memory block
  */
 char *create_array(unsigned int size, char c)
 {
 	char *mem;
+	int i;
 
 	mem = malloc(size * sizeof(c));
+
+	for (i = 0; i < size; ++i)
+	{
+		mem[i] = c;
+	}
+
 	return (mem);
 }
