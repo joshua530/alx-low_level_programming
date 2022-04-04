e19ac458faffe9cf05433e750ae12550d6de0d7f
joshua530 <joshuaomari5@gmail.com>
2022-04-04 21:37:04 +0300

    update documentation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,7 +7,8 @@
  * @width: the width of the array
  * @height: height of the array
  *
- * Description: instantiates an array
+ * Description: instantiates an array of arrays and initializes it with zeros
+ *
  * Return: returns a pointer to a 2 dimensional array of integers or NULL
  * if creation failed
  */
@@ -15,7 +16,7 @@ int **alloc_grid(int width, int height)
 {
 	int row, col;
 
-	if (width == 0 || height == 0)
+	if (width < 1 || height < 1)
 		return (NULL);
 
 	int **grid, *grid_row;
