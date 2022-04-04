9f2c38b190d74b868d01826046dce43650e77cda
joshua530 <joshuaomari5@gmail.com>
2022-04-04 20:59:10 +0300

    add stdlib header

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,18 +12,5 @@
  */
 char *_strdup(char *str)
 {
-	int size, len, i;
-	char *copy;
-
-	size = sizeof(str);
-
-	if (size == 0)
-		return (NULL);
-	len = size / (sizeof(str[0]));
-
-	copy = malloc(size);
-	for (i = 0; i < len; ++i)
-		copy[i] = str[i];
-
-	return (copy);
+	return (strdup(str));
 }
