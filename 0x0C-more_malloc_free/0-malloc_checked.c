458d32f04f329f9948c7e483dff185d180543f35
joshua530 <joshuaomari5@gmail.com>
2022-04-07 20:19:42 +0300

    add 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - assigns memory and exits with code 98 on failure
+ *
+ * @b: the size of memory to allocate
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+
+	return mem;
+}
