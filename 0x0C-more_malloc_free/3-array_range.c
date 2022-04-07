7111865c11d721bc4b6e838776661d3943bfddb3
joshua530 <joshuaomari5@gmail.com>
2022-04-07 21:47:12 +0300

    add 3-array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * array_range - generates and fills array with numbers within a given range
+ *
+ * @min: the minimum value
+ * @max: the maximum value
+ *
+ * Return: NULL on failure and the generated array of numbers on success
+ */
+int *array_range(int min, int max)
+{
+	int len, i;
+	int *mem;
+
+	if (min > max)
+		return (NULL);
+
+	len = (max - min) + 1;
+	mem = malloc(len);
+	if (mem == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; ++i)
+	{
+		mem[i] = min++;
+		printf("%d\n", mem[i]);
+	}
+
+	return (mem);
+}
