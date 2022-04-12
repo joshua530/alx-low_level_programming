549a95821a9ba54f38c123cd903c4717a099372b
joshua530 <joshuaomari5@gmail.com>
2022-04-12 14:21:18 +0300

    add 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+
+/**
+ * main - prints out name of current file
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < sizeof(__FILE__); ++i)
+	{
+		_putchar(__FILE__[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
