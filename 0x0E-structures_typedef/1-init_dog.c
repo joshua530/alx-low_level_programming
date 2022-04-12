389dcd32e0b4b80fd45f74dc7111a0d5f547869f
joshua530 <joshuaomari5@gmail.com>
2022-04-12 18:42:37 +0300

    add documentation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
 #include "dog.h"
 
+/**
+ * init_dog - instantiates a dog object
+ *
+ * @d: dog pointer
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
