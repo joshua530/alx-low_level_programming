4ef405b04b84050f66a3142215d4fc3970c6fd83
joshua530 <joshuaomari5@gmail.com>
2022-04-21 15:58:44 +0300

    correct compilation error

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node_end - appends a node to the end of a list
@@ -26,4 +27,33 @@ list_t *add_node_end(list_t **head, const char *str)
 		tmp->next = create_node(str);
 	}
 	return tmp;
+}
+
+/**
+ * create_node - creates a new node
+ *
+ * @str: the string to assign to new node
+ *
+ * Return: new node or NULL if creation failed
+ */
+list_t *create_node(const char *str)
+{
+	list_t *tmp = malloc(sizeof(list_t));
+
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	tmp->next = NULL;
+	if (str == NULL)
+	{
+		tmp->len = 0;
+		tmp->str = NULL;
+	}
+	else
+	{
+		tmp->str = strdup(str);
+		tmp->len = strlen(str);
+	}
+	return (tmp);
 }
