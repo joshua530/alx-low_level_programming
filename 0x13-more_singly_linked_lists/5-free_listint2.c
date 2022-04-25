bf96f8dad7a85727bd87f1a494ed55fe4b800cba
joshua530 <joshuaomari5@gmail.com>
2022-04-25 21:59:01 +0300

    implement free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees up memory used by a list. head is set to null
+ *
+ * @head: list's head node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *current;
+
+	current = *head;
+	while (current != NULL)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+
+	(*head) = NULL;
+}
