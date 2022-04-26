bb396967d6c46996aa783e869c8140348338193f
joshua530 <joshuaomari5@gmail.com>
2022-04-26 14:26:54 +0300

    refactor list insertion

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,16 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *current = *head;
-	listint_t *new;
+	listint_t *current = *head, *new;
+
+	if (head == NULL && idx == 0) /* list is currently empty, add head node */
+	{
+		new = malloc(sizeof(listint_t));
+		new->n = n;
+		new->next = NULL;
+		(*head) = new;
+		return (new);
+	}
 
 	while (current != NULL)
 	{
@@ -32,15 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		if (i == (idx - 1))
 		{
-			new = malloc(sizeof(listint_t));
-			if (new == NULL)
-			{
-				return (NULL);
-			}
-			new->next = current->next;
-			new->n = n;
-			current->next = new;
-			return (new);
+			return (insert_before(current, current->next, n));
 		}
 		else
 		{
@@ -49,4 +49,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 	return (NULL);
+}
+
+/**
+ * insert_before - inserts a node before another
+ *
+ * @before: parent node of current node
+ * @current: the node that will be preceded by the new node
+ * @value: value of the new node
+ *
+ * Return: inserted node if successful and NULL if insertion failed
+ */
+listint_t *insert_before(listint_t *before, listint_t *current, int value)
+{
+	listint_t *tmp = malloc(sizeof(listint_t));
+
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	tmp->n = value;
+	tmp->next = current;
+	before->next = tmp;
+	return (tmp);
 }
