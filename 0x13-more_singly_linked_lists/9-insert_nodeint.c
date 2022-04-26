1c0bd804b62168602b3abfc419084e1f2e41c478
joshua530 <joshuaomari5@gmail.com>
2022-04-26 13:32:47 +0300

    add insertion before head node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (current != NULL)
 	{
-		if (idx == 0)
+		if (idx == 0) /* insert before head node */
 		{
 			new = malloc(sizeof(listint_t));
 			if (new == NULL)
@@ -26,6 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			}
 			new->n = n;
 			new->next = current;
+			(*head) = new;
 			return (new);
 		}
 
