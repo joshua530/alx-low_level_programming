5f9bfd3b208b2e8db598f40bd3011607541a733f
joshua530 <joshuaomari5@gmail.com>
2022-04-28 21:54:45 +0300

    add 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_bit - gets bit at a given index
+ *
+ * @n: the binary number
+ * @index: index whose bit is to be found
+ *
+ * Return: value of the bit at the index or -1 if error
+ * occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+}
