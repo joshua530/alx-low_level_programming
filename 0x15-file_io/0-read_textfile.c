7b613d6b9b29701878b398e5a4eb9b65ae2dc86c
joshua530 <joshuaomari5@gmail.com>
2022-05-04 09:12:33 +0300

    check for read error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,23 +23,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	if (letters > SSIZE_MAX)
-	{
-		return (0);
-	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
 
-	buf = malloc(letters + 1 * sizeof(char));
+	buf = malloc(letters * sizeof(char));
 	if (buf == NULL)
 	{
 		return (0);
 	}
 	sz = read(fd, buf, letters);
+	if (sz == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 	if (sz != 0)
 	{
 		buf[sz + 1] = '\0';
